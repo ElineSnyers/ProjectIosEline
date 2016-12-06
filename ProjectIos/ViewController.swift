3c7d07c41a5cbca307488226fed5b27404e797b5
Eline Snyers <elinesnyers@ptr-gh30tn8rhht3favq9gq.18120a2.ip6.access.telenet.be>
2016-12-06 20:02:49 +0100

    Initial Commit

---
+++ ProjectIos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectIos
+//
+//  Created by Eline Snyers on 06-12-16.
+//  Copyright © 2016 Eline Snyers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
